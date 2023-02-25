0724dcde34b63236eefa4b7e7b7ad0d3bf547fe7
CielSsongsSeta <gch00536@naver.com>
2023-02-25 13:24:53 +0900

    2023.2.25 작업  Omok

---
+++ Omok/Omok.cpp
@@ -0,0 +1,34 @@
+﻿// Omok.cpp : 애플리케이션에 대한 진입점을 정의합니다.
+//
+
+#include "framework.h"
+#include "Omok.h"
+
+LRESULT CALLBACK DlgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+
+    if (msg == WM_PAINT) {
+
+    }
+    else if (msg == WM_CLOSE) {
+        DestroyWindow(hwnd);
+    }
+
+    return 0;
+}
+
+//윈도우 전용 main 함수 (시작점)
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+   
+    DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DlgProc);
+
+
+    return 0;
+}
+
+
+
+
